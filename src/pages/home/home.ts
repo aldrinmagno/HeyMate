2d236a2630e30bda20a611014f98cccc2119e752
aldrinmagno <aldrinmagno@gmail.com>
2018-08-28 00:27:27 +0930

    add interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
   }
 
