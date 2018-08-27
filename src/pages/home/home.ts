044d253bc4fa6223cbb763f3befb293ec77523ac
aldrinmagno <aldrinmagno@gmail.com>
2018-08-28 03:23:35 +0930

    add alert and message feature

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { HTTP } from "@ionic-native/http";
+import { text } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,48 @@ import { NavController, ToastController } from 'ionic-angular';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  public todos = [];
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: HTTP) {
+
+  }
+
+  sendMessage(message) {
+
+    this.todos.push(message);
+
+
+   /* this.http.get('http://ionic.io', {}, {})
+      .then(data => {
+
+        console.log(data.status);
+        console.log(data.data); // data received by server
+        console.log(data.headers);
+
+      })
+      .catch(error => {
+
+        console.log(error.status);
+        console.log(error.error); // error message as string
+        console.log(error.headers);
+
+      }); */
+  }
+
+  definition(word) {
+    let title = word;
+
+    let definitionAlert = this.alertCtrl.create({
+      title: title,
+      message: "sample definition",
+      buttons: [
+        { 
+          text: "Got It!"
+        }
+      ]
+    });
+
+    definitionAlert.present();
   }
 
 }
